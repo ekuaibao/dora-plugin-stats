0b1aadd99bec1a566a6ae87c00bd85331649ad70
niuhaifeng <nhf@ekuaibao.com>
2017-07-19 19:01:04 +0800

    提交代码

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+
+module.exports = {
+  name: 'stats',
+  middleware: function () {
+    const verbose = this.query.verbose
+    const compiler = this.get('compiler')
+    const arr = compiler._plugins.done
+    const statsOpts = compiler.options.stats
+    compiler._plugins.done = [
+      function (stats) {
+        if (verbose || stats.hasErrors()) {
+          console.log(stats.toString(statsOpts || {colors: true}))
+        }
+        if (arr) {
+          stats.toString = function () { return '' }
+          arr.forEach(function (fn) {
+            fn(stats)
+          })
+        }
+      }
+    ]
+    return function * (next) {
+      yield next
+    }
+  }
+}
